07b329b943590b05ae84c1084c15c1059eb7be85
Anurag Pathak <anuragpathak911@gmail.com>
2022-01-12 21:15:29 +0530

    fix: replaced href by Link and to fix router bug.

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,22 +1,23 @@
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 export default function NavigationBar() {
   return (
     <div>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/">Read-News</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Read-News</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/category/entertainment">Entertainment</NavDropdown.Item>
-                <NavDropdown.Item href="/category/sports">Sports</NavDropdown.Item>
-                <NavDropdown.Item href="/category/science">Science</NavDropdown.Item>
-                <NavDropdown.Item href="/category/technology">Technology</NavDropdown.Item>
-                <NavDropdown.Item href="/category/business">Business</NavDropdown.Item>
-                <NavDropdown.Item href="/category/health">Health</NavDropdown.Item>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <NavDropdown title="Categories" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/category/entertainment">Entertainment</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/category/sports">Sports</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/category/science">Science</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/category/technology">Technology</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/category/business">Business</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/category/health">Health</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
