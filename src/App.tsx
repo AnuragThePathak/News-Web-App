736b9fd48dedf460c9d1b72b2c67692c3647a37e
Anurag Pathak <anuragpathak911@gmail.com>
2022-01-14 21:18:21 +0530

    fix: Error boundary wrapped.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { lazy, Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
+import ErrorBoundary from "./ErrorBoundary"
 const Home = lazy(() => import("./components/Home"))
 const MainLayout = lazy(() => import("./components/MainLayout"))
 const NewsList = lazy(() => import("./components/NewsList"))
@@ -11,18 +12,20 @@ function App() {
 
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>} >
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home country={country} />} />
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>} >
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home country={country} />} />
 
-            <Route path="category/:category" element={<NewsList country={country} />} />
-          </Route>
+              <Route path="category/:category" element={<NewsList country={country} />} />
+            </Route>
 
-          <Route path="*" element={<NoMatch message=
-            "The page you are looking for doesn't exist." />} />
-        </Routes>
-      </Suspense>
+            <Route path="*" element={<NoMatch message=
+              "The page you are looking for doesn't exist." />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   )
 }
