068506c360b1ac42de7833c803b222fc255df75d
Anurag Pathak <anuragpathak911@gmail.com>
2022-01-14 18:59:18 +0530

    feat: Lazy loading and Error boundaries.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { lazy, Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
-import Home from "./components/Home"
-import MainLayout from "./components/MainLayout"
-import NewsList from "./components/NewsList"
-import NoMatch from "./components/NoMatch"
+const Home = lazy(() => import("./components/Home"))
+const MainLayout = lazy(() => import("./components/MainLayout"))
+const NewsList = lazy(() => import("./components/NewsList"))
+const NoMatch = lazy(() => import("./components/NoMatch"))
 
 function App() {
   const country = "in"
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home country={country} />} />
+      <Suspense fallback={<div>Loading...</div>} >
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home country={country} />} />
 
-          <Route path="category/:category" element={<NewsList country={country}/>} />
-        </Route>
+            <Route path="category/:category" element={<NewsList country={country} />} />
+          </Route>
 
-        <Route path="*" element={<NoMatch message=
-          "The page you are looking for doesn't exist." />} />
-      </Routes>
+          <Route path="*" element={<NoMatch message=
+            "The page you are looking for doesn't exist." />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
